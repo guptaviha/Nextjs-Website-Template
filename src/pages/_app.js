8bb42423bef39a3e4af1ff6cda2f18f2fcea9c3b
Viha Gupta <vihagupta95@gmail.com>
2025-05-26 19:27:58 -0400

    added anlytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,19 @@
 import '../styles/main.css'
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
 
-  return getLayout(<Component {...pageProps} />);
+  return getLayout(
+    <>
+      <Component {...pageProps} />
+      <Script
+        async
+        src="www.analytics.vihagupta.com"
+        data-website-id="daf0599d-b7ea-4697-bdf9-98dbb59ea77c"
+      />
+    </>
+  );
 }
 
 export default MyApp;
